f7f15d4610a81365cff24c1a714e176d1598342f
egisatriaa <egi.satriadyw@gmail.com>
2025-11-16 19:53:53 +0700

    Update contact form

--- src/App.jsx
+++ src/App.jsx
@@ -113,22 +113,31 @@ function App() {
                 <p className="text-base/loose text-center mb-10 opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
                     Mari terhubung dengan saya
                 </p>
-                <form action="https://formsubmit.co/egi.satriadyw@gmail.com" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+                <form action="https://api.web3forms.com/submit" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+                    {/* ACCESS KEY WAJIB */}
+                    <input type="hidden" name="access_key" value="05bab85e-2f0e-488e-ac90-01430bffd014" />
+
                     <div className="flex flex-col gap-6">
+                        {/* NAMA */}
                         <div className="flex flex-col gap-2">
                             <label className="font-semibold">Nama lengkap</label>
-                            <input type="text" name="nama" placeholder="Masukkan Nama..." className="border border-zinc-500 p-2 rounded-md" required />
+                            <input type="text" name="name" placeholder="Masukkan Nama..." className="border border-zinc-500 p-2 rounded-md" required />
                         </div>
+
+                        {/* EMAIL */}
                         <div className="flex flex-col gap-2">
                             <label className="font-semibold">Email</label>
                             <input type="email" name="email" placeholder="Masukkan Email..." className="border border-zinc-500 p-2 rounded-md" required />
                         </div>
+
+                        {/* PESAN */}
                         <div className="flex flex-col gap-2">
                             <label htmlFor="pesan" className="font-semibold">
-                                message
+                                Message
                             </label>
-                            <textarea name="pesan" id="pesan" cols="45" rows="7" placeholder="Masukkan Pesan..." className="border border-zinc-500 p-2 rounded-md" required></textarea>
+                            <textarea name="message" id="pesan" cols="45" rows="7" placeholder="Masukkan Pesan..." className="border border-zinc-500 p-2 rounded-md" required></textarea>
                         </div>
+
                         <div className="text-center">
                             <button type="submit" className="bg-violet-700 p-3 rounded-lg w-full cursor-pointer border border-zinc-500 hover:bg-violet-600">
                                 Send Message
